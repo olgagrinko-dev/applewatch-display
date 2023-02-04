e872c1d17b9e84cf9028625433e49ce7db94da0a
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-02-04 23:30:35 +0300

    project

---
+++ app.js
@@ -0,0 +1,29 @@
+// Описание проекта:
+// Сверстать статичный макет с использованием препроцессоров. 
+// Добавить динамику в часы и год
+// Составляющие проекта:
+// querySelector; innerHTML; new Date; setInterval / setTimeout
+
+const time = document.getElementById(`time`);
+
+function doClock(time_) {
+    let date = new Date();
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+
+    if (hours.length < 2) {
+        hours = `0` + hours;
+    }
+    if (minutes.length < 2) {
+        minutes = `0` + minutes;
+    }
+    if (seconds.length < 2) {
+        seconds = `0` + seconds;
+    }
+
+    let clockString = hours + `:` + minutes + `:` + seconds;
+    time.textContent = clockString;
+}
+doClock(time)
+setInterval(doClock, 1000)
