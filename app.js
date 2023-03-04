86c2f465cbc97fecf404648d7034cbaf8c4a8c10
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-03-04 15:54:14 +0300

    adding to project year

--- app.js
+++ app.js
@@ -4,26 +4,34 @@
 // Составляющие проекта:
 // querySelector; innerHTML; new Date; setInterval / setTimeout
 
-const time = document.getElementById(`time`);
-
-function doClock() {
-    let date = new Date();
-    let hours = date.getHours().toString();
-    let minutes = date.getMinutes().toString();
-    let seconds = date.getSeconds().toString();
-
-    if (hours.length < 2) {
-        hours = `0` + hours;
-    }
-    if (minutes.length < 2) {
-        minutes = `0` + minutes;
-    }
-    if (seconds.length < 2) {
-        seconds = `0` + seconds;
+class Test {
+    constructor() {
+        this.time = document.getElementById(`time`);
+        setInterval(this.doClock, 1000);
     }
 
-    let clockString = hours + `:` + minutes + `:` + seconds;
-    time.textContent = clockString;
+    doClock() {
+        let date = new Date();
+        let hours = date.getHours().toString();
+        let minutes = date.getMinutes().toString();
+        let seconds = date.getSeconds().toString();
+        let year = date.getFullYear().toString();
+
+        if (hours.length < 2) {
+            hours = `0` + hours;
+        }
+        if (minutes.length < 2) {
+            minutes = `0` + minutes;
+        }
+        if (seconds.length < 2) {
+            seconds = `0` + seconds;
+        }
+
+        let clockString = hours + `:` + minutes + `:` + seconds;
+        this.time.textContent = clockString;
+
+        document.querySelector(`.watch p`).innerHTML = year;
+    }
 }
 
-setInterval(doClock, 1000)
+const test = new Test();
