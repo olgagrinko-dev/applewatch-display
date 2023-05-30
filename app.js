92250fa3f07258e06d68458d3412e6cad8070992
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-05-30 22:19:28 +0300

    task

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ class Test {
         if (hours.length < 2) {
             hours = `0` + hours;
         }
+
         if (minutes.length < 2) {
             minutes = `0` + minutes;
         }
+        
         if (seconds.length < 2) {
             seconds = `0` + seconds;
         }
